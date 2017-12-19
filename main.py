f4d6c86498690cc19a8a64741518a92282ad9fc3
DewitteNick <nickdewitte@hotmail.com>
2017-12-19 16:43:50 +0100

    Added a more precise calculation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import snmp
 import time
-
+import math
 
 
 def getPlcStartTime():
@@ -11,17 +11,23 @@ def getPlcStartTime():
     return startTime + plcErrorCorrection
 
 def getCurrentGMT():
-    return int(time.time() // 1)
+    # return int(time.time() // 1)
+    return int(round(time.time(), 0))
 
 
-
+def getPlcStartTimeAccurate(precision=10):  # PLC may give a small delay sometimes, multiple querries subdue this effect
+    totalResult = 0
+    for i in range(precision):
+        totalResult += getPlcStartTime()
+    return int(round(totalResult / precision))
 
 
 
 
 
 def main():
-    print(getPlcStartTime())
+    print("quick estimate:", getPlcStartTime())
+    print("Acurate calculation:", getPlcStartTimeAccurate(10))
 
 
 if __name__ == "__main__":
